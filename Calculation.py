2407866d0102768da58e3cdf66d15c9cb2c49cb6
MelthePassionateCoder <melliza.limbago31@gmail.com>
2023-03-04 15:53:21 +0800

    simple bills calculation

---
+++ Calculation.py
@@ -0,0 +1,7 @@
+# My electricity bills for the last three months have been 
+# $23, $32 and $64. What is the 
+# average monthly electricity bill over the three month period? 
+# Write an expression to calculate the mean, and use print() 
+# to view the result.
+monthly_bill = ( 23 + 32 + 64 ) / 3
+print(monthly_bill)
